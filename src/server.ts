08eb67b2656aa8d1d15550448d42adc834e4689f
haseennm <hasinmmpm786@gmail.com>
2026-02-26 10:19:06 +0530

    basic files

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import Fastify from 'fastify'
+import { env } from './utils/env'
+import multipart from '@fastify/multipart'
+import { companyRoutes } from './module/company/company.router'
+
+const app = Fastify({
+    logger: false,
+})
+app.register(multipart)
+app.register(companyRoutes, { prefix: '/company' })
+
+const start = async () => {
+    try {
+        await app.listen({ port: Number(env.PORT) })
+        console.log("\x1b[44m Server running.. \x1b[0m");
+
+    } catch (err) {
+        // app.log.error(err)
+        console.log(err)
+        process.exit(1)
+    }
+}
+start()
